db7f92e2dfda8666d772f00bd7b0cd796338b6c4
helenarodmun <helenarodmun@gmail.com>
2023-06-13 17:20:36 +0100

    formularios

--- 04-formularios/src/components/FormularioComponent.js
+++ 04-formularios/src/components/FormularioComponent.js
@@ -13,7 +13,8 @@ const FormularioComponent = () => {
             name: data.name.value,
             last_name: data.last_name.value,
             genre: data.genre.value,
-            bio: data.bio.value
+            bio: data.bio.value,
+            submit: data.submit.value
         };
 
         console.log(user);
@@ -21,55 +22,60 @@ const FormularioComponent = () => {
     };
 
     const changeData = e => {
-        let input_name = e.target.name;
-        let user_update = user;
-
-        // user_update[input_name] = e.target.value;
-        // console.log(user_update)
-
+        var input_name = e.target;
+        var user_update = user;
+        //Modificar el estado para que se establezca el estado, capturar el estado previo
+        setUser(previous_state => ({
+                ...previous_state,
+                [input_name]: e.target.value//sobreescribe la propiedad que se está modificando         
+        })    
+        );
+        console.log(user);
     };
+
+
 
     return (
         <div>
             <h1>Formularios con React</h1>
 
-            {user.bio && user.name.length >= 1 && 
+            {user.submit && 
                 <div className='info label label-grey'>
                     {user.name} {user.last_name} es un  {user.genre} y su biografía es esta: <p>
                         {user.bio}</p>
                 </div>
-            
+
             }
 
             <form onSubmit={getDataForm}>
                 <input type='text'
                     name='name'
                     placeholder='Nombre'
-                    onChange={ changeData }
-                    >
+                    onChange={changeData}
+                >
 
                 </input>
                 <input type='text'
                     name='last_name'
                     placeholder='Apellido'
-                    onChange={ changeData }
-                    >
+                    onChange={changeData}
+                >
 
                 </input>
                 <select name='genre'
-                 onChange={ changeData }
-                 >
-                    <option value='man'>Hombre</option>
-                    <option value='woman'>Mujer</option>
+                    onChange={changeData}
+                >
+                    <option value='hombre'>Hombre</option>
+                    <option value='mujer'>Mujer</option>
                 </select>
-                <textarea placeholder='Biografía' 
-                name='bio'
-                onChange={ changeData }
+                <textarea placeholder='Biografía'
+                    name='bio'
+                    onChange={changeData}
                 >
 
                 </textarea>
 
-                <input type='submit' value='Enviar'></input>
+                <input type='submit' value='Enviar' name='submit'></input>
             </form>
         </div>
     )
